4d17803d1677275e36370cba1b90054c22eb7142
Dindayalsingh04 <dindyalsingh2310@gmail.com>
2024-12-23 14:46:21 +0530

    made a automation project

---
+++ main.py
@@ -0,0 +1,41 @@
+from twilio.rest import Client
+from datetime import datetime, timedelta
+import time
+
+account_sid = "AC209ec194834edbc615051fb7b9b1bb7e"
+auth_token = "b7a2fb7463eca9cd2b1929dae1533fbf"
+
+client = Client(account_sid, auth_token)
+
+def send_whatsapp_message(recipient_number, message_body):
+    try:
+        message = client.messages.create(
+            from_='whatsapp:+14155238886',
+            body=message_body,
+            to=f'whatsapp:{recipient_number}'
+        )
+        print(f"Message sent Successfully to {recipient_number} ! Message SID: {message.sid}")
+    except Exception as e:
+        print(f"Error sending message to {recipient_number}: {str(e)}")
+
+name = input("Enter the recipient name: ")
+recipient_number = input("Enter the recipient whatsapp number with country code: ")
+message_body = input(f'Enter the message for {name}: ')
+
+date_str = input("Enter the date in YYYY-MM-DD format: ")
+time_str = input("Enter the time in HH:MM:SS format: ")
+
+schedule_datetime = datetime.strptime(date_str + " " + time_str, "%Y-%m-%d %H:%M:%S")
+current_datetime = datetime.now()
+time_difference = schedule_datetime - current_datetime
+delay_seconds = time_difference.total_seconds()
+
+if delay_seconds <= 0:
+    print("The scheduled time has already passed.")
+else:
+    print(f'Message scheduled to be sent to {name} at {schedule_datetime}.')
+
+
+    time.sleep(delay_seconds)
+
+    send_whatsapp_message(recipient_number, message_body)
